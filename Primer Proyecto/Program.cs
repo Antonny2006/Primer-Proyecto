2d14cd84ad8b8b01c92014c0ccd644ff02eb751c
Antonny2006 <lemuelgomezz@outlook.com>
2024-08-15 13:33:24 -0600

    Actualizacion de cambios en la clase program
    Recibir y convertir datos

--- Primer Proyecto/Program.cs
+++ Primer Proyecto/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hola Mundo");
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double EdadMeses, Edad;
+        string nombre;
+
+        Console.WriteLine("Bienvenidos a nuestro primer programa creado en C#");
+        Console.WriteLine("Ingrese su nombre para continuar");
+        nombre = Console.ReadLine(); // Para Recibir Valores de Caracter
+        Console.WriteLine("Ingrese su edad");
+        Edad = Convert.ToDouble(Console.ReadLine()); // Convierte la entrada a double
+        EdadMeses = Edad * 12; //Calculos matematicos
+        Console.WriteLine("La edad en meses es: " + EdadMeses); //Concatenacion
+    }
+}
